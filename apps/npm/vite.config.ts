2a2140d019502661954061dd2baeeb2ec960caa4
Diwanshu <saidiwanshu1880@gmail.com>
2024-04-26 00:08:33 +0530

    Fixed The Vite Build

--- apps/npm/vite.config.ts
+++ apps/npm/vite.config.ts
@@ -2,17 +2,19 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
+    minify: true,
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "lib/main.tsx"),
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external: ["react", "react/jsx-runtime", "react-dom"],
     },
+    
   },
 });
