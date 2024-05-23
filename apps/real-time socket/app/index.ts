caf0373ac3e1fa3be412943fc451f90da888dae3
bwestwood11 <dabrettwestwood@gmail.com>
2024-05-23 09:45:18 -0400

    Co-authored-by: Diwanshu Midha <Diwanshumidha@users.noreply.github.com>

--- apps/real-time socket/app/index.ts
+++ apps/real-time socket/app/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
-import { Village, villages } from './socket';
+import { Village, rooms, villages } from './socket';
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -18,22 +18,60 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log(`A User Connected`, socket.id);
 
-  socket.on('join', (data) => {
-    const {villageId, role} = data
+  socket.on('join', (data:{villageId:string, role:"consumer" | "agent",userName:string}) => {
+    const {villageId, role, userName} = data
     // Check if the village exists
-    const village = villages.find(v => v.villageId === villageId);
+    let village = villages.find(v => v.villageId === villageId);
     if (village) {
       console.log(`User joined village: ${village.name}`);
       socket.emit(`User joined village: ${village.name}`)
 
     } else {
-      const village = new Village(villageId, "Village ABC")
+      village = new Village(villageId, "Village ABC", io)
       console.log(village.villageId)
       socket.emit(`User created & joined village: ${village.name}`)
     }
 
+    if(role === "consumer"){
+      village?.joinConsumer(socket.id, userName)
+
+
+      const waitlist = village?.getWaitlistUsers()
+      const agents = village?.getAgents()
+
+      console.log({waitlist,agents});
+
+
+      if(agents){
+        io.to(agents).emit("consumer-joined", {users:waitlist})
+      }
+     
+    }
+    if(role === "agent"){
+      village.joinAgent(socket.id, userName)
+    }
+    
 
   });
+
+  socket.on('make-room', (data:{villageId:string,consumerSocketId:string})=>{
+    const village = villages.find(v => v.villageId === data.villageId);
+    if(!village){
+      return
+    }
+    village.makeRoom(socket.id, data.consumerSocketId)
+
+  })
+
+  socket.on('send-message', (data:{roomId:string,message:string})=>{
+    console.log(data)
+    const room = rooms.find(room => room.roomId === data.roomId)
+    if(!room || !room.roomId ){
+      return
+    }
+    room?.sendMessage(socket,data.roomId, data.message)
+
+  })
 
   
 
--- apps/real-time socket/app/socket.ts
+++ apps/real-time socket/app/socket.ts
@@ -1,4 +1,8 @@
+import { Server, Socket } from "socket.io";
+
 export const villages: Village[] = [];
+// Fix this make a socketHandler
+export const rooms:Room[] = []
 
 
 
@@ -8,14 +12,16 @@ export class Village {
     public consumer_waitlist:Consumer[]
     public available_Agents:Agent[]
     public rooms:Room[]
+    public io:Server
     
 
-    constructor(villageId: string, name: string) {
+    constructor(villageId: string, name: string, io:Server) {
         this.villageId = villageId;
         this.name = name;
         this.consumer_waitlist = []
         this.available_Agents = []
         this.rooms = []
+        this.io = io
 
         villages.push(this);
         console.log("Yeah You entered the village");
@@ -34,6 +40,33 @@ export class Village {
         const agent = new Agent(socketId,name)
         this.available_Agents.push(agent)
     }
+
+
+
+    getAgents(){
+        const agents = this.available_Agents
+        return agents.map(agent => agent.socketId)
+    }
+
+    getWaitlistUsers(){
+        const users = this.consumer_waitlist
+        return users.map(user => ({name:user.consumerName, socketId:user.socketId}))
+    }
+
+
+    makeRoom(agentId:string,consumerId:string){
+        const agent = this.available_Agents.find((agent) => agent.socketId === agentId)
+        const consumer = this.consumer_waitlist.find((consumer)=>consumer.socketId === consumerId)
+
+        if(!agent || !consumer){
+            return
+        }
+
+        const room = new Room(agent,consumer, this)
+        rooms.push(room)
+    }
+
+    
 
 
 
@@ -70,13 +103,37 @@ class Consumer {
 }
 
 class Room {
-    // public agent:Agent
-    // public consumer:Consumer
+    public agent:Agent
+    public consumer:Consumer
+    private village:Village
+    public roomId:string | null
 
-    // constructor(agentId,consumerId){
-    //     this.agent = socketId
-    //     this.consumerName = consumerName
 
-    // }
+    constructor(agent:Agent,consumer:Consumer, village:Village){
+        this.agent = agent
+        this.consumer = consumer
+        this.village = village
 
+        const io = village.io
+        const socket = io.sockets.sockets.get(agent.socketId) 
+        if(!socket){
+            this.roomId = null
+            return
+        }
+        const roomId = `room-${agent.socketId}-${consumer.socketId}`;
+
+        console.log(roomId,this.agent,this.consumer)
+        socket.join(roomId)
+        io.to(consumer.socketId).socketsJoin(roomId);
+        socket.to(consumer.socketId).emit("agent-joined", { roomId, agentId: agent.socketId });
+
+        this.roomId = roomId
+    }
+
+
+    sendMessage(socket: Socket, roomId: string, message: string) {
+        this.village.io.to(roomId).emit("message", { senderId: socket.id, message });
+    }
+
+    
 }
