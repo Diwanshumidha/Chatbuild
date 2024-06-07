7422e40237529755f63a2f520f9b6e5bc271bfea
bwestwood11 <dabrettwestwood@gmail.com>
2024-06-07 09:11:30 -0400

    change

--- packages/chatbot/src/lib/constants.ts
+++ packages/chatbot/src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_PATH = "https://chatbuild.io/api";
-export const SOCKET_SERVER_PATH = 'http://socket.chatbuild.io:8080'
+export const BASE_PATH = "https://www.chatbuild.io/api";
+export const SOCKET_SERVER_PATH = 'http://socket.chatbuild.io'
 // export const SOCKET_SERVER_PATH = "http://localhost:8080";
