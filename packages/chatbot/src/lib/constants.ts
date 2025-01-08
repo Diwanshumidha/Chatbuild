67dd1e1d6340fc0468c414b53f459b92a34dd13f
Brett <dabrettwestwood@gmail.com>
2025-01-08 09:59:49 -0500

    update chatbuild packages

--- packages/chatbot/src/lib/constants.ts
+++ packages/chatbot/src/lib/constants.ts
@@ -1,4 +1,4 @@
-// export const BASE_PATH = "https://www.chatbuild.io/api";
-export const BASE_PATH = "http://localhost:3000/api";
+export const BASE_PATH = "https://www.chatbuild.io/api";
+// export const BASE_PATH = "http://localhost:3000/api";
 export const SOCKET_SERVER_PATH = 'https://socket.chatbuild.io'
 // export const SOCKET_SERVER_PATH = "http://localhost:8080";
